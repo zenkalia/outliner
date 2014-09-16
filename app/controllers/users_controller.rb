dbb420bf4b8b308a0a773da17ecf3d3dc0a8d006
Mike Kendall <zenkalia@gmail.com>
2014-09-16 05:43:27 +0000

    :phone: email based authentication will never die

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def create
-    User.where(params.require(:user).permit(:email)).first_or_create
+    user = User.where(params.require(:user).permit(:email)).first_or_create
+    session[:user_id] = user.id
+    redirect_to '/'
+  end
+
+  def sign_out
+    session[:user_id] = nil
+    redirect_to '/'
   end
 end
